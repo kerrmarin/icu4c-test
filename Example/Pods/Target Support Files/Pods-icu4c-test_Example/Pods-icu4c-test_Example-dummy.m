88b89af7bdaf658f57718503ffc0928b671ce574
Kerr Marin Miller <kerr@kerrmarin.com>
2021-01-20 09:23:06 +0000

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-icu4c-test_Example/Pods-icu4c-test_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_icu4c_test_Example : NSObject
+@end
+@implementation PodsDummy_Pods_icu4c_test_Example
+@end
---
+++ Example/Pods/Target Support Files/icu4c-test/icu4c-test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_icu4c_test : NSObject
+@end
+@implementation PodsDummy_icu4c_test
+@end
